850a1d9cf1a2dc8fefe7d13750dcfe92d7a02507
Ching Chow <ycangchow@gmail.com>
2017-11-14 18:12:22 +0800

    add firebase

--- fotosort/src/app/app.component.ts
+++ fotosort/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
 import { environment } from '../environments/environment';
+import * as picasa from 'picasa';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,39 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'app';
+  title = 'FotoNize';
 
   constructor(){
+    firebase.initializeApp({
+            apiKey: "AIzaSyDnwljBbcXSBNz_SgqTICaZ6B6Rg5PJr0g",
+            authDomain: "noonewillnotice-2e8e6.firebaseapp.com",
+            databaseURL: "https://noonewillnotice-2e8e6.firebaseio.com",
+            projectId: "noonewillnotice-2e8e6",
+            storageBucket: "noonewillnotice-2e8e6.appspot.com",
+            messagingSenderId: "276231888981"
+        });
   }
 
   ngOnInit(){
 
   }
+
+  signUp(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    firebase.auth()
+            .signInWithPopup(provider)
+            .then(function(result) {
+            // This gives you a Google Access Token.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+
+            console.log(result);
+            }).catch((err)=>{
+              console.log(err);
+            });
+  }
+
 }
--- fotosort/src/app/app.module.ts
+++ fotosort/src/app/app.module.ts
@@ -7,19 +7,34 @@ import { PhotosService } from './photos.service';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDnwljBbcXSBNz_SgqTICaZ6B6Rg5PJr0g",
+      authDomain: "noonewillnotice-2e8e6.firebaseapp.com",
+      databaseURL: "https://noonewillnotice-2e8e6.firebaseio.com",
+      projectId: "noonewillnotice-2e8e6",
+      storageBucket: "noonewillnotice-2e8e6.appspot.com",
+      messagingSenderId: "276231888981"
     }), 
     AngularFireDatabaseModule, 
     AngularFireAuthModule
   ],
-  providers: [PhotosService,],
+  providers: [PhotosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

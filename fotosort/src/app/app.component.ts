3bea2a36f4185602ff2310b939352f492ca85e52
Ching Chow <ycangchow@gmail.com>
2017-11-14 15:56:45 +0800

    firebase 1st try

--- fotosort/src/app/app.component.ts
+++ fotosort/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  constructor(){
+    firebase.initializeApp({
+            apiKey: process.env.APIKEY,
+            authDomain: process.env.AUTHDOMAIN,
+            databaseURL: process.env.DATABASEURL,
+            projectId: process.env.PROJECTID,
+            storageBucket: process.env.STORAGEBUCKET,
+            messagingSenderId: process.env.MESSAGINGSENDERID
+        });
+  }
+
+  ngOnInit(){
+
+  }
 }
--- fotosort/src/app/app.module.ts
+++ fotosort/src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotosService } from './photos.service';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import dotenv from 'dotenv'
+dotenv.config()
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: process.env.APIKEY,
+      authDomain: process.env.AUTHDOMAIN,
+      databaseURL: process.env.DATABASEURL,
+      projectId: process.env.PROJECTID,
+      storageBucket: process.env.STORAGEBUCKET,
+      messagingSenderId: process.env.MESSAGINGSENDERID
+    }), 
+    AngularFireDatabaseModule, 
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [PhotosService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

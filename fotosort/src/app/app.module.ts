5691c8940431265c69634de7ffd60ca2e8210d28
Ching Chow <ycangchow@gmail.com>
2017-11-14 15:58:17 +0800

    afa

--- fotosort/src/app/app.module.ts
+++ fotosort/src/app/app.module.ts
@@ -7,12 +7,9 @@ import { PhotosService } from './photos.service';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-<<<<<<< HEAD
 
 import dotenv from 'dotenv'
 dotenv.config()
-=======
->>>>>>> 434fd95d6484f6d2ddad2553db267b858797d368
 
 @NgModule({
   declarations: [
@@ -20,7 +17,6 @@ dotenv.config()
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     AngularFireModule.initializeApp({
       apiKey: process.env.APIKEY,
       authDomain: process.env.AUTHDOMAIN,
@@ -31,8 +27,6 @@ dotenv.config()
     }), 
     AngularFireDatabaseModule, 
     AngularFireAuthModule
-=======
->>>>>>> 434fd95d6484f6d2ddad2553db267b858797d368
   ],
   providers: [PhotosService,],
   bootstrap: [AppComponent]

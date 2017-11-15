65e0ddf62117b8257a7f2e22e2903c9496397ef7
Ching Chow <ycangchow@gmail.com>
2017-11-15 15:17:37 +0800

    start full stack

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const reload = require('reload');
+const watch = require('watch');
+const app = express();
+
+app.get('/login',(req,res)=>{
+    res.json({"key":"Hello World!"});
+});
+
+reloadServer = reload(app);
+watch.watchTree(__dirname + "/frontend", function (f, curr, prev) {
+    // Fire server-side reload event 
+    reloadServer.reload();
+});
+
+app.use(express.static('frontend'));
+
+app.use(function(req, res, next) {
+    res.sendFile(__dirname + "/frontend/index.html");
+})
+
+
+app.listen(8080);

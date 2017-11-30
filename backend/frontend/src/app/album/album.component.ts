c2af7e0b0f0eed8de643d2c9e274643787fa629b
Ching Chow <ycangchow@gmail.com>
2017-11-30 19:31:22 +0800

    album components changed

--- backend/frontend/src/app/album/album.component.ts
+++ backend/frontend/src/app/album/album.component.ts
@@ -33,11 +33,46 @@ export class AlbumComponent implements OnInit {
 
   disabled:boolean = true;
 
-  constructor(private http:Http) { }
+  constructor(private http:Http) { 
+    this.imagesAll.forEach((all)=>{
+      this.imagesArraySubscription = Observable.of(null).delay(500).subscribe(() => {
+        this.imagesArraySubscribed = all;
+      });
+    })
+    // this.imagesArraySubscription = Observable.of(null).delay(500).subscribe(() => {
+    //   this.imagesArraySubscribed = this.imagesArray;
+    // });
+  }
 
   ngOnInit() {
     this.getAlbum();
   }
+
+    openModalWindow:boolean = false; 
+  
+    imagePointer: number = 0;
+  
+    openModalWindowObservable: boolean = false;
+    
+    imagePointerObservable: number = 0;
+  
+    imagesAll:any = [];
+
+    private subscription: Subscription;
+    private imagesArraySubscription: Subscription;
+  
+      // observable of an array of images with a delay to simulate a network request
+        imagesArraySubscribed: Array<Image>;
+      
+        customDescription: Description = {
+          imageText: 'Look this image ',
+          numberSeparator: ' of ',
+          beforeTextDescription: ' => '
+        };
+      
+        customFullDescription: Description = {
+          customFullDescription: 'Custom description of the current visible image',
+        };
 
   getAlbum(){
     this.http.get('/albumDB')
@@ -61,19 +96,38 @@ export class AlbumComponent implements OnInit {
 
 
 checkAlbum(albumID){
-  let result = this.albums.filter(album=>(album.id ===albumID));
-    if (albumID ===result[0].id ){
-    //  this.filteredAlbums.push(albumID)
-    return this.filteredAlbums.push(albumID)
-   }else{
-    //  this.filteredAlbums =[]
-    this.filteredAlbums=[]
+    if(this.filteredAlbums.includes(albumID)){
+      let result = this.albums.filter(album=>{
+                      return album.id == albumID})
+                      .map((album)=>{
+                      return album.id
+                    });
+                    console.log(result);
+                    this.filteredAlbums = result;
+    }else{
+      this.filteredAlbums.push(albumID)
+    }
+  }
+
+  getPhotoFromFotosort(){
+    while(this.imagesAll.length>0){
+      this.imagesAll.pop()
+    }
+    this.filteredAlbums.forEach((filteredAlbum)=>{
+      this.albums.forEach((album)=>{
+        if(album.albumID == filteredAlbum){
+          let newArray = album.images.map(image=>{
+            return new Image(image,image,null,image)
+          })
+          let newInstance = Observable.of(newArray).delay(300)
+          this.imagesAll.push({title: album.albumName, image: newInstance})
+           }
+        })
+      })
    }
-}
 
   deleteAlbum(id){
     this.http.delete('/deletealbum/'+id)
-    
     .subscribe(
        (response)=>{
          response.json().forEach((album)=>{
@@ -94,7 +148,61 @@ checkAlbum(albumID){
       this.albums = this.albums.filter((n)=>{
       return n.albumID !=id
       })
-    
-     console.log(this.albums)
+      console.log(this.albums)
     }
+
+  openImageModal(image: Image) {
+      this.imagePointer = this.imagesArray.indexOf(image);
+      this.openModalWindow = true;
+    }
+  
+  openImageModalObservable(image: Image) {
+      this.imagesAll.forEach((all)=>{
+        this.subscription = all.subscribe((val: Image[]) => {
+          this.imagePointerObservable = val.indexOf(image);
+          this.openModalWindowObservable = true;
+        });
+      })
+    }
+  
+    onImageLoaded(event: ImageModalEvent) {
+      console.log('onImageLoaded action: ' + Action[event.action]);
+      console.log('onImageLoaded result:' + event.result);
+    }
+  
+    onVisibleIndex(event: ImageModalEvent) {
+      this.customFullDescription.customFullDescription = `Custom description of visible image with index= ${event.result}`;
+      console.log('action: ' + Action[event.action]);
+      console.log('result:' + event.result);
+    }
+  
+    onIsFirstImage(event: ImageModalEvent) {
+      console.log('onfirst action: ' + Action[event.action]);
+      console.log('onfirst result:' + event.result);
+    }
+  
+    onIsLastImage(event: ImageModalEvent) {
+      console.log('onlast action: ' + Action[event.action]);
+      console.log('onlast result:' + event.result);
+    }
+  
+    onCloseImageModal(event: ImageModalEvent) {
+      console.log('onClose action: ' + Action[event.action]);
+      console.log('onClose result:' + event.result);
+      this.openModalWindow = false;
+      this.openModalWindowObservable = false;
+    }
+  
+    ngOnDestroy() {
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+      }
+      if(this.imagesArraySubscription) {
+        this.imagesArraySubscription.unsubscribe();
+      }
+    }
+  
+
   }
+
+  
